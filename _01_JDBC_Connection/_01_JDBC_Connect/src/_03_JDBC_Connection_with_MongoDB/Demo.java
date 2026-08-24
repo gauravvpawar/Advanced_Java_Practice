5cb6a408a3b69a681efa298ea2ffa23b223bcee1
Gaurav <gauravkailaspawar101@gmail.com>
2026-08-25 00:00:01 +0530

    initialise with mongoDB

--- _01_JDBC_Connection/_01_JDBC_Connect/src/_03_JDBC_Connection_with_MongoDB/Demo.java
+++ _01_JDBC_Connection/_01_JDBC_Connect/src/_03_JDBC_Connection_with_MongoDB/Demo.java
@@ -1,15 +1,24 @@
 package _03_JDBC_Connection_with_MongoDB;
 
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		
 		try
 		{
-			Class.forName("mongodb.jdbc.MongoDriver");
-			System.out.println("Connection Successfull");
-			
+			   
+			   MongoClient mongoClient =
+	                    MongoClients.create("mongodb://localhost:27017");
 
+	            MongoDatabase database =
+	                    mongoClient.getDatabase("college");
+
+	            System.out.println("Connection Successful");
 		}catch(Exception e)
 		{
 			System.out.println("Something went wrong");
